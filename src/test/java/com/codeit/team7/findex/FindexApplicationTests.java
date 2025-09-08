9f1b168f03a418b80ce7fc200c090bd1eea491b7
parkjoohwan <jookid94@gmail.com>
2025-09-08 18:20:03 +0900

    remove : local tc

--- src/test/java/com/codeit/team7/findex/FindexApplicationTests.java
+++ src/test/java/com/codeit/team7/findex/FindexApplicationTests.java
@@ -1,19 +1,13 @@
 package com.codeit.team7.findex;
 
-import com.codeit.team7.findex.util.OpenApiUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FindexApplicationTests {
 
-  @Autowired
-  OpenApiUtil openApiUtil;
-
   @Test
   void contextLoads() {
-    System.out.println(openApiUtil.fetchStockMarketIndex(null));
   }
 
 }
