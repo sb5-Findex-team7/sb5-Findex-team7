96b654e7be6b4119bf0dcb7a77e1c01592cb74d8
Jelo777 <junheef1@gmail.com>
2025-09-09 10:28:51 +0900

    feat : IndexInfo JPA 매핑

--- src/main/java/com/codeit/team7/findex/domain/entity/IndexInfo.java
+++ src/main/java/com/codeit/team7/findex/domain/entity/IndexInfo.java
@@ -3,16 +3,29 @@ package com.codeit.team7.findex.domain.entity;
 import com.codeit.team7.findex.domain.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "index_info")
+@Table(
+    name = "index_info",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"idx_csf", "idx_nm"})
+    }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class IndexInfo extends BaseUpdatableEntity {
 
-  @Column(nullable = false, name = "idx_csf")
+  @Column(nullable = false, name = "idx_csf", length = 300)
   private String indexClassification;
-  @Column(nullable = false, name = "idx_nm")
+  @Column(nullable = false, name = "idx_nm", length = 200)
   private String indexName;
   @Column(nullable = false, name = "itms_cnt")
   private Integer itemCount;
@@ -20,10 +33,40 @@ public class IndexInfo extends BaseUpdatableEntity {
   private LocalDate basePointInTime;
   @Column(nullable = false, name = "bas_idx")
   private Integer baseIndex;
-  @Column(nullable = false, name = "source_type")
-  private String sourceType;
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, name = "source_type", length = 20)
+  private SourceType sourceType;
+  // 즐겨찾기 기본값 false
   @Column(nullable = false, name = "favorite")
-  private Boolean favorite;
-  @Column(name = "enabled")
-  private Boolean enabled;
+  private boolean favorite;
+  // 활성화 기본값 true
+  @Column(nullable = false, name = "enabled")
+  private boolean enabled = true;
+
+  public IndexInfo(String indexClassification, String indexName, Integer itemCount,
+      LocalDate basePointInTime, Integer baseIndex, SourceType sourceType, boolean favorite,
+      boolean enabled) {
+    this.indexClassification = indexClassification;
+    this.indexName = indexName;
+    this.itemCount = itemCount;
+    this.basePointInTime = basePointInTime;
+    this.baseIndex = baseIndex;
+    this.sourceType = sourceType;
+    this.favorite = favorite;
+    this.enabled = enabled;
+  }
+
+  public void update(Integer newItemCount, LocalDate newBasePointInTime, Integer newBaseIndex,
+      boolean newFavorite) {
+    if (newItemCount != null && !newItemCount.equals(this.itemCount)) {
+      this.itemCount = newItemCount;
+    }
+    if (newBasePointInTime != null && !newBasePointInTime.equals(this.basePointInTime)) {
+      this.basePointInTime = newBasePointInTime;
+    }
+    if (newBaseIndex != null && !newBaseIndex.equals(this.baseIndex)) {
+      this.baseIndex = newBaseIndex;
+    }
+    this.favorite = newFavorite;
+  }
 }
