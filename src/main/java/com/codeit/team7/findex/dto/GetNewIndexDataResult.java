fdb7acb56372182bde3771f4a8de58668f24342c
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2025-09-10 10:34:09 +0900

    feat : 지수데이터 연동 서비스 구축 완료

---
+++ src/main/java/com/codeit/team7/findex/dto/GetNewIndexDataResult.java
@@ -0,0 +1,20 @@
+package com.codeit.team7.findex.dto;
+
+import com.codeit.team7.findex.dto.response.StockMarketIndexResponse.Item;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class GetNewIndexDataResult {
+
+  Map<Long, List<Item>> items;
+  boolean isToUpdate;
+  LocalDate baseFromDate;
+  LocalDate baseToDate;
+}
