90ef35349bac8e35c1837665d1ef1cef35a3a500
Jelo777 <junheef1@gmail.com>
2025-09-11 15:07:47 +0900

    [feat] index info (#9)
    * feat : IndexInfo JPA 매핑
    
    * refactor : IndexInfo 엔티티 baseIndex를 BigDecimal로 변경
    
    * [hotfix] : baseIndex 자료형 수정(Integer > BigDecimal) (#4)
    
    * fix : baseIndex 자료형 수정(Integer > BigDecimal)
    
    * fix : DDL 수정 (INT > NUMERIC)
    
    * feat : dto 생성
    
    * feat : repository생성 및 service(페이지네이션 조회 제외 나머지)
    
    * feat : CusorPageResponseIndexInfoDto
    
    * feat : IndexInfo Repository
    
    * feat : IndexInfo 페이지 정렬조회 서비스 추가
    
    * feat : IndexInfoController
    
    * chore :
    
    ---------
    
    Co-authored-by: Jade <jookid94@gmail.com>

--- src/main/java/com/codeit/team7/findex/domain/entity/IndexInfo.java
+++ src/main/java/com/codeit/team7/findex/domain/entity/IndexInfo.java
@@ -1,30 +1,81 @@
 package com.codeit.team7.findex.domain.entity;
 
 import com.codeit.team7.findex.domain.entity.base.BaseUpdatableEntity;
+import com.codeit.team7.findex.domain.enums.SourceType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "index_info")
+@Table(
+    name = "index_info",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"idx_csf", "idx_nm"})
+    }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class IndexInfo extends BaseUpdatableEntity {
 
-  @Column(nullable = false, name = "idx_csf")
+  @Column(nullable = false, name = "idx_csf", length = 300)
   private String indexClassification;
-  @Column(nullable = false, name = "idx_nm")
+
+  @Column(nullable = false, name = "idx_nm", length = 200)
   private String indexName;
+
   @Column(nullable = false, name = "itms_cnt")
   private Integer itemCount;
+
   @Column(nullable = false, name = "bas_pntm")
   private LocalDate basePointInTime;
-  @Column(nullable = false, name = "bas_idx")
+
+  @Column(nullable = false, name = "bas_idx", precision = 32, scale = 2)
   private BigDecimal baseIndex;
-  @Column(nullable = false, name = "source_type")
-  private String sourceType;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, name = "source_type", length = 20)
+  private SourceType sourceType;
+
+  // 즐겨찾기 기본값 false
   @Column(nullable = false, name = "favorite")
-  private Boolean favorite;
-  @Column(name = "enabled")
-  private Boolean enabled;
+  private boolean favorite;
+
+  // 활성화 기본값 true
+  @Column(nullable = false, name = "enabled")
+  private boolean enabled = true;
+
+  public IndexInfo(String indexClassification, String indexName, Integer itemCount,
+      LocalDate basePointInTime, BigDecimal baseIndex, SourceType sourceType, boolean favorite,
+      boolean enabled) {
+    this.indexClassification = indexClassification;
+    this.indexName = indexName;
+    this.itemCount = itemCount;
+    this.basePointInTime = basePointInTime;
+    this.baseIndex = baseIndex;
+    this.sourceType = sourceType;
+    this.favorite = favorite;
+    this.enabled = enabled;
+  }
+
+  public void update(Integer newItemCount, LocalDate newBasePointInTime, BigDecimal newBaseIndex,
+      boolean newFavorite) {
+    if (newItemCount != null && !newItemCount.equals(this.itemCount)) {
+      this.itemCount = newItemCount;
+    }
+    if (newBasePointInTime != null && !newBasePointInTime.equals(this.basePointInTime)) {
+      this.basePointInTime = newBasePointInTime;
+    }
+    if (newBaseIndex != null && !newBaseIndex.equals(this.baseIndex)) {
+      this.baseIndex = newBaseIndex;
+    }
+    this.favorite = newFavorite;
+  }
 }
